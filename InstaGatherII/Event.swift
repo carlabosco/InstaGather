050f7d43632bc10fb7252a015d29bfc309cc83bb
Carla Bosco <carlabosco@gmail.com>
2019-07-02 21:29:42 -0700

    Add classes to first 2 view controllers

---
+++ InstaGatherII/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  InstaGatherII
+//
+//  Created by Carla Bosco on 02.07.19.
+//  Copyright © 2019 Carla Bosco. All rights reserved.
+//
+
+import UIKit
+
+class Event: NSObject {
+
+}
