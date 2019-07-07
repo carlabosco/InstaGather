9a7304437225ca87f787ad405133fa9523720f2f
Carla Bosco <carlabosco@gmail.com>
2019-07-07 15:32:50 -0700

    Add class to scene #3

---
+++ InstaGatherII/FormStep3ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormStep3ViewController.swift
+//  InstaGatherII
+//
+//  Created by Carla Bosco on 07.07.19.
+//  Copyright © 2019 Carla Bosco. All rights reserved.
+//
+
+import UIKit
+
+class FormStep3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
