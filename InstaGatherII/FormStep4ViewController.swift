41acea86983d4d2b54f8652dfb386dffa81d2754
Carla Bosco <carlabosco@gmail.com>
2019-07-08 12:37:15 -0700

    Add scene and class 4

---
+++ InstaGatherII/FormStep4ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormStep4ViewController.swift
+//  InstaGatherII
+//
+//  Created by Carla Bosco on 08.07.19.
+//  Copyright © 2019 Carla Bosco. All rights reserved.
+//
+
+import UIKit
+
+class FormStep4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
